7d019b8772bfc3082b81ffd14236e2d79731cc0f
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-04-02 14:04:27 +0200

    todo: testin

--- circuit/src/contract-registry/src/lib.rs
+++ circuit/src/contract-registry/src/lib.rs
@@ -2,17 +2,16 @@
 
 use codec::{Decode, Encode};
 use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch, traits::Get};
-use frame_system;
+use frame_system::{self, ensure_root};
 
-// #[cfg(test)]
-// mod mock;
+#[cfg(test)]
+mod mock;
 
-// #[cfg(test)]
-// mod tests;
+#[cfg(test)]
+mod tests;
 
 pub trait Config: frame_system::Config {
     type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
-    type Error: From<Error<Self>> + Into<<Self as frame_system::Config>::Error>;
 }
 
 #[derive(PartialEq, Eq, Encode, Decode, Default, Clone)]
@@ -25,8 +24,7 @@ pub struct ComposableContract {
 
 decl_storage! {
     trait Store for Module<T: Config> as ContractRegistry {
-        // https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-        // ( requester: AccountId, contract_name: Vec<u8> ) -> ComposableContract
+        /// ( requester, contract_name ) -> Option<ComposableContract>
         Registry get(fn registry):
           double_map hasher(blake2_128_concat) T::AccountId, hasher(blake2_128_concat) Vec<u8> => Option<ComposableContract>;
     }
@@ -37,42 +35,48 @@ decl_event!(
     where
         AccountId = <T as frame_system::Config>::AccountId,
     {
-        // params [who, contract_name]
+        // Event parameters [requester, contract_name]
         ComposableContractStored(AccountId, Vec<u8>),
+        // Event parameters [requester, contract_name]
+        ComposableContractPurged(AccountId, Vec<u8>),
     }
 );
 
 decl_error! {
-    pub enum Error for Module<T: Config> {
-        /// There is no contract for the given requester and contract name.
-        NoSuchContract,
-    }
+    pub enum Error for Module<T: Config> {}
 }
+
+// HELP: https://github.com/paritytech/substrate/blob/master/frame/example/src/lib.rs
 
 decl_module! {
     pub struct Module<T: Config> for enum Call where origin: T::Origin {
         type Error = Error<T>;
-    }
-}
+        fn deposit_event() = default;
 
-// https://stackoverflow.com/questions/56902167/in-substrate-is-there-a-way-to-use-storage-and-functions-from-one-custom-module?rq=1
-// TODO: get rid of anyhow
-impl<T: Config> Module<T> {
-    pub fn get_contract(
-        requester: T::AccountId,
-        contract_name: Vec<u8>,
-    ) -> Result<ComposableContract, T::Error> {
-        if let Some(contract) = <Registry<T>>::get(requester, contract_name) {
-            Ok(contract)
-        } else {
-            Err(Error::<T>::NoSuchContract)
+        /// Inserts a contract to the on-chain registry. Root only access.
+        /// TODO weight
+        #[weight = 10_419]
+        pub fn insert_contract(origin, requester: T::AccountId, contract_name: Vec<u8>, contract:ComposableContract) -> dispatch::DispatchResult {
+            ensure_root(origin)?;
+
+            <Registry<T>>::insert(&requester, &contract_name, contract);
+
+            Self::deposit_event(RawEvent::ComposableContractStored(requester, contract_name));
+
+            Ok(())
+        }
+
+        /// Removes a contract from the on-chain registry. Root only access.
+        /// TODO weight
+        #[weight = 10_419]
+        pub fn remove_contract(origin, requester: T::AccountId, contract_name: Vec<u8>) -> dispatch::DispatchResult {
+            ensure_root(origin)?;
+
+            <Registry<T>>::remove(&requester, &contract_name);
+
+            Self::deposit_event(RawEvent::ComposableContractPurged(requester, contract_name));
+
+            Ok(())
         }
     }
-    // pub fn put_contract(
-    //     requester: T::AccountId,
-    //     contract_name: Vec<u8>,
-    //     contract: ComposableContract,
-    // ) -> Result<(), <Module<T> as Config>::Error> {
-    //     panic!("not implemented");
-    // }
 }
