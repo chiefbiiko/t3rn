bfe66b69d100142dde493255b800c11e6ce60a15
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-04-04 13:17:54 +0200

    style: simpler tests

--- circuit/src/contract-registry/src/tests.rs
+++ circuit/src/contract-registry/src/tests.rs
@@ -8,7 +8,7 @@ fn it_inserts_a_contract_into_the_registry() {
         // Fast forwarding to block#2 because block#1 does not include events.
         run_to_block(2);
 
-        let dispatch_result = ContractRegistryModule::store_contract(
+        assert_ok!(ContractRegistryModule::store_contract(
             Origin::root(),
             REQUESTER,
             contract_name(),
@@ -17,24 +17,21 @@ fn it_inserts_a_contract_into_the_registry() {
                 bytes: bytes(),
                 abi: None,
             },
+        ));
+
+        assert!(<Registry<Test>>::contains_key(REQUESTER, contract_name()));
+
+        assert_eq!(
+            System::events(),
+            vec![EventRecord {
+                phase: Phase::Initialization,
+                event: Event::pallet_contract_registry(RegistryEvent::<Test>::ContractStored(
+                    REQUESTER,
+                    contract_name(),
+                )),
+                topics: vec![],
+            }]
         );
-
-        assert_ok!(dispatch_result);
-
-        let exists = <Registry<Test>>::contains_key(REQUESTER, contract_name());
-
-        assert!(exists);
-
-        let expected_events = vec![EventRecord {
-            phase: Phase::Initialization,
-            event: Event::pallet_contract_registry(RegistryEvent::<Test>::ContractStored(
-                REQUESTER,
-                contract_name(),
-            )),
-            topics: vec![],
-        }];
-
-        assert_eq!(System::events(), expected_events);
     });
 }
 
@@ -44,7 +41,7 @@ fn it_inserts_idempotent() {
         // Fast forwarding to block#2 because block#1 does not include events.
         run_to_block(2);
 
-        let dispatch_result = ContractRegistryModule::store_contract(
+        assert_ok!(ContractRegistryModule::store_contract(
             Origin::root(),
             REQUESTER,
             contract_name(),
@@ -53,11 +50,9 @@ fn it_inserts_idempotent() {
                 bytes: bytes(),
                 abi: None,
             },
-        );
+        ));
 
-        assert_ok!(dispatch_result);
-
-        let dispatch_result = ContractRegistryModule::store_contract(
+        assert_storage_noop!(ContractRegistryModule::store_contract(
             Origin::root(),
             REQUESTER,
             contract_name(),
@@ -66,21 +61,19 @@ fn it_inserts_idempotent() {
                 bytes: bytes(),
                 abi: None,
             },
+        ));
+
+        assert_eq!(
+            System::events(),
+            vec![EventRecord {
+                phase: Phase::Initialization,
+                event: Event::pallet_contract_registry(RegistryEvent::<Test>::ContractStored(
+                    REQUESTER,
+                    contract_name(),
+                )),
+                topics: vec![],
+            }]
         );
-
-        assert_ok!(dispatch_result);
-        // TODO: investigate the actual checkin mechanism 4 assert_storage_noop
-
-        let expected_events = vec![EventRecord {
-            phase: Phase::Initialization,
-            event: Event::pallet_contract_registry(RegistryEvent::<Test>::ContractStored(
-                REQUESTER,
-                contract_name(),
-            )),
-            topics: vec![],
-        }];
-
-        assert_eq!(System::events(), expected_events);
     });
 }
 
@@ -90,7 +83,7 @@ fn it_removes_a_contract_from_the_registry() {
         // Fast forwarding to block#2 because block#1 does not include events.
         run_to_block(2);
 
-        let dispatch_result = ContractRegistryModule::store_contract(
+        assert_ok!(ContractRegistryModule::store_contract(
             Origin::root(),
             REQUESTER,
             contract_name(),
@@ -99,39 +92,37 @@ fn it_removes_a_contract_from_the_registry() {
                 bytes: bytes(),
                 abi: None,
             },
+        ));
+
+        assert_ok!(ContractRegistryModule::purge_contract(
+            Origin::root(),
+            REQUESTER,
+            contract_name()
+        ));
+
+        assert!(!<Registry<Test>>::contains_key(REQUESTER, contract_name()));
+
+        assert_eq!(
+            System::events(),
+            vec![
+                EventRecord {
+                    phase: Phase::Initialization,
+                    event: Event::pallet_contract_registry(RegistryEvent::<Test>::ContractStored(
+                        REQUESTER,
+                        contract_name(),
+                    )),
+                    topics: vec![],
+                },
+                EventRecord {
+                    phase: Phase::Initialization,
+                    event: Event::pallet_contract_registry(RegistryEvent::<Test>::ContractPurged(
+                        REQUESTER,
+                        contract_name(),
+                    )),
+                    topics: vec![],
+                },
+            ]
         );
-
-        assert_ok!(dispatch_result);
-
-        let dispatch_result =
-            ContractRegistryModule::purge_contract(Origin::root(), REQUESTER, contract_name());
-
-        assert_ok!(dispatch_result);
-
-        let exists = <Registry<Test>>::contains_key(REQUESTER, contract_name());
-
-        assert!(!exists);
-
-        let expected_events = vec![
-            EventRecord {
-                phase: Phase::Initialization,
-                event: Event::pallet_contract_registry(RegistryEvent::<Test>::ContractStored(
-                    REQUESTER,
-                    contract_name(),
-                )),
-                topics: vec![],
-            },
-            EventRecord {
-                phase: Phase::Initialization,
-                event: Event::pallet_contract_registry(RegistryEvent::<Test>::ContractPurged(
-                    REQUESTER,
-                    contract_name(),
-                )),
-                topics: vec![],
-            },
-        ];
-
-        assert_eq!(System::events(), expected_events);
     });
 }
 
@@ -141,7 +132,7 @@ fn it_removes_idempotent() {
         // Fast forwarding to block#2 because block#1 does not include events.
         run_to_block(2);
 
-        let dispatch_result = ContractRegistryModule::store_contract(
+        assert_ok!(ContractRegistryModule::store_contract(
             Origin::root(),
             REQUESTER,
             contract_name(),
@@ -150,48 +141,48 @@ fn it_removes_idempotent() {
                 bytes: bytes(),
                 abi: None,
             },
+        ));
+
+        assert_ok!(ContractRegistryModule::purge_contract(
+            Origin::root(),
+            REQUESTER,
+            contract_name()
+        ));
+
+        assert_storage_noop!(ContractRegistryModule::purge_contract(
+            Origin::root(),
+            REQUESTER,
+            contract_name()
+        ));
+
+        assert_eq!(
+            System::events(),
+            vec![
+                EventRecord {
+                    phase: Phase::Initialization,
+                    event: Event::pallet_contract_registry(RegistryEvent::<Test>::ContractStored(
+                        REQUESTER,
+                        contract_name(),
+                    )),
+                    topics: vec![],
+                },
+                EventRecord {
+                    phase: Phase::Initialization,
+                    event: Event::pallet_contract_registry(RegistryEvent::<Test>::ContractPurged(
+                        REQUESTER,
+                        contract_name(),
+                    )),
+                    topics: vec![],
+                },
+            ]
         );
-
-        assert_ok!(dispatch_result);
-
-        let dispatch_result =
-            ContractRegistryModule::purge_contract(Origin::root(), REQUESTER, contract_name());
-
-        assert_ok!(dispatch_result);
-
-        let dispatch_result =
-            ContractRegistryModule::purge_contract(Origin::root(), REQUESTER, contract_name());
-
-        assert_ok!(dispatch_result);
-        // TODO: investigate the actual checkin mechanism 4 assert_storage_noop
-
-        let expected_events = vec![
-            EventRecord {
-                phase: Phase::Initialization,
-                event: Event::pallet_contract_registry(RegistryEvent::<Test>::ContractStored(
-                    REQUESTER,
-                    contract_name(),
-                )),
-                topics: vec![],
-            },
-            EventRecord {
-                phase: Phase::Initialization,
-                event: Event::pallet_contract_registry(RegistryEvent::<Test>::ContractPurged(
-                    REQUESTER,
-                    contract_name(),
-                )),
-                topics: vec![],
-            },
-        ];
-
-        assert_eq!(System::events(), expected_events);
     });
 }
 
 // #[test]
 // fn it_stores_contracts_separately_per_requester() {
-    // generate storage keys for same contract but different requesters..
-    // ..and assert these keys are different
+// generate storage keys for same contract but different requesters..
+// ..and assert these keys are different
 //     new_test_ext().execute_with(|| {});
 // }
 
