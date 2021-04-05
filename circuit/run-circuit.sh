e718c712fc4518031bcbd56010fc16ab5da08853
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-04-05 16:50:14 +0200

    cleanup: archive

--- circuit/run-circuit.sh
+++ circuit/run-circuit.sh
@@ -7,20 +7,5 @@ cargo build || exit
 echo "[run-circuit.sh] Cleaning the circuit dev database with './target/debug/circuit purge-chain --dev'..."
 yes | ./target/debug/circuit purge-chain --dev
 
-cd ./src/archive
-cargo build || exit
-cd ../..
-
-base_path=$HOME/.local/share/io.t3rn.circuit
-
 echo "[run-circuit.sh] Running circuit with './target/debug/circuit --dev -lruntime=debug'..."
-./target/debug/circuit \
-  --dev \
-  -lruntime=debug \
-  --pruning=archive \
-  --base-path=$base_path
-
-DATABASE_URL=postgres://user:pass@localhost:5432/archive \
-CHAIN_DATA_DB=$base_path/chains/dev/db \
-./src/archive/debug/archive \
-  TODO
+./target/debug/circuit --dev -lruntime=debug
